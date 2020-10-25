925b2c3187cd2c97d5920174b228e3c4736cf341
lim <tbnsok40@naver.com>
2020-10-26 03:51:02 +0900

    수정

---
+++ myproject/myproject/my_settings.py
@@ -0,0 +1,9 @@
+EMAIL = {
+    'EMAIL_BACKEND': 'django.core.mail.backends.smtp.EmailBackend',
+    'EMAIL_USE_TLS': True,
+    'EMAIL_PORT': 587,
+    'EMAIL_HOST': 'smtp.gmail.com',
+    # 요런 식으로 이메일 보낼! 계정 입력하시면돼요(위에서 설정했던계정)
+    'EMAIL_HOST_USER': 'tbnsok40@likelion.org',
+    'EMAIL_HOST_PASSWORD': 'tjd00gn!!',
+}
